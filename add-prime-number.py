97868d6a2e917578c7e2e036f73f413243d89c37
rakesh95136 <rakesh95136@yahoo.com>
2017-10-03 18:32:22 -0700

    identify prime number

---
+++ add-prime-number.py
@@ -0,0 +1,24 @@
+#Write a program that given number is a prime number
+
+x = 2
+
+i = 2
+while (i <= (x - 1)):
+	if (x%i) == 0:
+		break
+		pass
+	i += 1
+	#print(i,x)
+		
+
+
+
+if (i == x ):
+	print('PRIME')
+else:
+	print('NOT PRIME')
+
+		
+	
+
+
